4862064309e27ce1eaa4ec6b885418fa49bfb04a
Štefan Pekník <xpekni01@stud.fit.vutbr.cz>
2023-04-17 22:39:20 +0200

    create Tcp server

---
+++ ipkcpd/ipkcpd/TcpClientHandler.cs
@@ -0,0 +1,126 @@
+﻿using System.Net.Sockets;
+
+namespace ipkcpd;
+
+internal class TcpClientHandler : IDisposable
+{
+    private readonly TcpClient _client;
+    private readonly NetworkStream _stream;
+    private readonly StreamReader _reader;
+    private readonly StreamWriter _writer;
+
+    private bool _disposed;
+
+    public TcpClientHandler(TcpClient client)
+    {
+        _client = client;
+
+        // Get a stream object for reading and writing
+        _stream = _client.GetStream();
+
+        // Create a reader and writer for the stream.
+        _reader = new StreamReader(_stream);
+        _writer = new StreamWriter(_stream);
+    }
+
+
+    private void SayResult(TextWriter writer, int result)
+    {
+        Console.WriteLine($"Result: {result}");
+        writer.WriteLine($"RESULT {result}");
+        writer.Flush();
+    }
+
+
+    public void HandleClient()
+    {
+        try
+        {
+            var message =
+                // get HELLO
+                _reader.ReadLine();
+            if (message != "HELLO")
+            {
+                Console.WriteLine("Communication did not start with HELLO, ending connection.");
+                Dispose();
+                return;
+            }
+
+            Console.WriteLine("On HELLO answering with HELLO.");
+            _writer.WriteLine("HELLO");
+            _writer.Flush();
+
+            // Enter the message reading loop.
+            while (true)
+            {
+                message = _reader.ReadLine();
+                if (message is null)
+                {
+                    Console.WriteLine("Message was empty.");
+                    Dispose();
+                    return;
+                }
+
+                Console.WriteLine($"Received: {message}");
+
+                if (!message.StartsWith("SOLVE "))
+                {
+                    Console.WriteLine("Expression did not start with \"SOLVE \", ending connection.");
+                    Dispose();
+                    return;
+                }
+
+                var expression = message[6..]; // remove "SOLVE "
+
+                // Solve the request
+                try
+                {
+                    var result = new ExpressionSolver().Solve(expression);
+                    if (result < 0)
+                    {
+                        Console.WriteLine("Expression was negative.");
+                        Dispose();
+                        return;
+                    }
+
+                    SayResult(_writer, result);
+                }
+                catch (InvalidExpressionException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Dispose();
+                    return;
+                }
+            }
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("Client must have remotely close the connection.");
+        }
+    }
+
+    public void Dispose()
+    {
+        if (_disposed) return;
+
+        // Send last BYE
+        Console.WriteLine("Ending with BYE.");
+        try
+        {
+            _writer.WriteLine("BYE");
+            _writer.Flush();
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Client must have remotely close the connection.");
+        }
+
+        // Dispose of resources
+        _writer.Dispose();
+        _reader.Dispose();
+        _stream.Dispose();
+        _client.Dispose();
+
+        _disposed = true;
+    }
+}
---
+++ ipkcpd/ipkcpd/TcpServer.cs
@@ -0,0 +1,64 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ipkcpd;
+
+public class TcpServer : IServer
+{
+    private TcpListener? _tcpListener;
+    private readonly List<TcpClientHandler> _handlers = new();
+    private bool _disposed = false;
+
+    public void Run(string ip, int port)
+    {
+        try
+        {
+            var localAddress = IPAddress.Parse(ip);
+
+            // Create a server.
+            _tcpListener = new TcpListener(localAddress, port);
+
+            // Start listening for client requests.
+            _tcpListener.Start();
+
+            Console.WriteLine($"TCP server started. Listening on {localAddress}:{port}...");
+
+            // Enter the listening loop.
+            while (! _disposed)
+            {
+                Console.WriteLine("Waiting for a connection...");
+
+                // Perform a blocking call to accept requests.
+                var client = _tcpListener.AcceptTcpClient();
+                if (_disposed) break;
+                Console.WriteLine("Connected!");
+
+                // Create a new task to handle the client connection asynchronously.
+                var handler = new TcpClientHandler(client);
+                _handlers.Add(handler);
+                Task.Run(handler.HandleClient);
+            }
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine($"SocketException: {e}");
+        }
+    }
+
+
+
+    public void Dispose()
+    {
+        if (_disposed) return;
+        // Stop all the clients
+        foreach (var handler in _handlers)
+        {
+            handler.Dispose();
+        }
+
+        // Stop listening for new clients.
+        _tcpListener?.Stop();
+
+        _disposed = true;
+    }
+}
